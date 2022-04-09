9e0acbd4e35c9e83472e8b2a3c48d3be6560e851
Tomislav Mikšik <64797760+tomislavmiksik@users.noreply.github.com>
2022-04-09 22:33:57 +0200

    commit

--- app/src/main/java/com/example/kalkulatormiksik/MainActivity.kt
+++ app/src/main/java/com/example/kalkulatormiksik/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.kalkulatormiksik
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kalkulatormiksik.databinding.ActivityMainBinding
+import java.math.RoundingMode
+import java.text.DecimalFormat
+import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setTitle("BMI Calculator")
+
+        binding.button.setOnClickListener {
+            val df = DecimalFormat("#.##")
+            df.roundingMode = RoundingMode.CEILING
+            val weight = binding.weight?.text.toString().toDouble()
+            val height = binding.height?.text.toString().toDouble()
+
+            val bmi = (weight / (height * height)*10000)
+            binding.bmi?.text = df.format(bmi)
+        }
+
     }
 }
