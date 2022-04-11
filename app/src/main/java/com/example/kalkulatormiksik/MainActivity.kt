20619bea3310cfce342d9a89d3682bc1b43cf625
Tomislav Mikšik <tomislav.miksik@gmail.com>
2022-04-11 11:32:19 +0200

    commit

--- app/src/main/java/com/example/kalkulatormiksik/MainActivity.kt
+++ app/src/main/java/com/example/kalkulatormiksik/MainActivity.kt
@@ -2,30 +2,65 @@ package com.example.kalkulatormiksik
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import androidx.constraintlayout.widget.ConstraintSet.VISIBLE
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.CompoundButton
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
 import com.example.kalkulatormiksik.databinding.ActivityMainBinding
 import java.math.RoundingMode
 import java.text.DecimalFormat
-import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var isDarkTheme = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setTitle("BMI Calculator")
-        binding.button.setOnClickListener {
-            val df = DecimalFormat("#.##")
-            df.roundingMode = RoundingMode.CEILING
-            val weight = binding.weight?.text.toString().toDouble()
-            val height = binding.height?.text.toString().toDouble()
 
-            val bmi = (weight / (height * height)*10000)
-            binding.bmi?.text = df.format(bmi)
+        isDarkTheme = AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES
+
+        binding.apply {
+            button.setOnClickListener {
+                val df = DecimalFormat("#.##")
+                df.roundingMode = RoundingMode.CEILING
+                setTheme(R.style.Theme_MaterialComponents_Light)
+                if (weight.text.isEmpty() || height.text.isEmpty()) {
+                    bmi.text = getString(R.string.submitError)
+                    return@setOnClickListener
+                } else {
+                    val weight = weight.text.toString().toDouble()
+                    val height = height.text.toString().toDouble()
+                    val bmi = weight / (height * height)
+                    binding.bmi.text = df.format(bmi)
+                }
+            }
+
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+
+        val nightMode = menu?.findItem(R.id.themeSwitchText)
+        nightMode?.title =
+            if (isDarkTheme) getString(R.string.switchToLightTheme) else getString(R.string.switchToDarkTheme)
+        nightMode?.setOnMenuItemClickListener {
+            isDarkTheme = !isDarkTheme
+            if (isDarkTheme) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                nightMode.title = getString(R.string.switchToLightTheme)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+            recreate()
+            true
         }
 
+        return true
     }
+
 }
